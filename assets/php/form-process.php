384152043244f6078f1a42894677dae2e3510738
Igor Vilinchuk <ivilinchuk@gmail.com>
2018-06-30 12:13:35 +0800

    Testing php form

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -1,3 +1,10 @@
 <?
-	php echo $_POST["data"]; 
+	if($_POST)
+	
+
+	$to_email = "ivilinchuk@gmail.com";
+	$subject = "Testing form";
+	$message_body = $_POST["data"];
+	$headers = 'From: ivilinchuk@gmail.com';
+	mail($to_email, $subject, $message_body, $headers);
 ?>
